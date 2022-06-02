ed5b811e7b17a3f0394d6a6e408f7ef4a05b0a6f
BioLionMM <superpikapika@gmail.com>
2022-06-02 10:53:34 +0200

    creation BD ventes

---
+++ client.sql
@@ -0,0 +1,12 @@
+-- authentification ou connexion dans la console de xamp en ouvrant mysql et apache
+>mysql -u root --si pas de mot de passe
+mysqm -u root -p motdepasse
+
+
+-- lister toutes les bases de donnees
+SHOW DATABASES;
+
+-- 1 creation de la base de donnees
+
+CREATE DATABASE ventes;
+--2 creation de la table client
